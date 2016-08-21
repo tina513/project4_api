956c8f6612069d4d55026fa3a07d0b541b75f973
tina513 <dlnaz.ai@hotmail.com>
2016-08-21 15:30:25 -0400

    small changes for user friendly purpose

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,4 @@
-class RecipesController < ProtectedController
+class RecipesController < OpenReadController
   before_action :set_recipe, only: [:show, :update, :destroy]
   # before_action :check_validation,:only => [:create]
 
