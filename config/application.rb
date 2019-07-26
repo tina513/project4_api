96d09e2b7201fb28698954a8f29f704dfc73b23e
daishanjiang3 <daishanjiang3@gatech.edu>
2019-07-26 00:44:44 -0400

    Update cors origin

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module RailsApiTemplate
     # Cross-Origin Resource Sharing
     config.middleware.use Rack::Cors do
       allow do
-        origins ENV['CLIENT_URL'] || 'https://tina513.github.io'
+        origins ENV['CLIENT_URL'] || 'https://tina513.github.io/project4_client_ag/'
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
